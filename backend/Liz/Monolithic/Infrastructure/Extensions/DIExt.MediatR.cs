9c8132ba33f8344dc2ad23b91f9b76d25725bc00
Balenciaga69 <Balenciaga69@users.noreply.github.com>
2025-08-17 12:14:59 +0000

    chore: auto format backend code after PR to develop

--- backend/Liz/Monolithic/Infrastructure/Extensions/DIExt.MediatR.cs
+++ backend/Liz/Monolithic/Infrastructure/Extensions/DIExt.MediatR.cs
@@ -1,6 +1,6 @@
-﻿using Infrastructure.Behaviors;
+﻿using System.Reflection;
+using Infrastructure.Behaviors;
 using MediatR;
-using System.Reflection;
 
 namespace Monolithic.Infrastructure.Extensions;
 
